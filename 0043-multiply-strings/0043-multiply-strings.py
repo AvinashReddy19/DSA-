8c69b86b63667ff17e3285c371c0a3ef926590e7
Aviansh Reddy Challa <115349366+AvinashReddy19@users.noreply.github.com>
2024-06-02 17:26:02 +0530

    Time: 26 ms (98.75%), Space: 16.4 MB (91.05%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,3 +1,20 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        return str(int(num1)*int(num2))
+        if num1 == '0' or num2 == '0':
+            return '0'
+        
+        def decode(num):
+            ans = 0
+            for i in num:
+                ans = ans*10 +(ord(i) - ord('0'))
+            return ans
+
+        def encode(s):
+            news = ''
+            while s:
+                a = s % 10
+                s = s // 10
+                news = chr(ord('0') + a) + news
+            return news
+        
+        return encode(decode(num1)*decode(num2))
