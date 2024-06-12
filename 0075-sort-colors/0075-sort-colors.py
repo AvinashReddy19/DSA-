a0c922c46f0de4af5583b0a5474a35b46e8905f4
Aviansh Reddy Challa <115349366+AvinashReddy19@users.noreply.github.com>
2024-06-12 23:27:29 +0530

    Time: 35 ms (73.49%), Space: 16.5 MB (79.02%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,17 +1,4 @@
 class Solution:
     def sortColors(self, arr: List[int]) -> None:
-        start,mid=0,0
-        end=len(arr)-1
-        while mid <= end:
-            if arr[mid] == 0:
-                arr[start], arr[mid] = arr[mid], arr[start]
-                start += 1
-                mid += 1
-            elif arr[mid] == 2:
-                arr[mid], arr[end] = arr[end], arr[mid]
-                end -= 1
-            else:  # arr[mid] == 1
-                mid += 1
-
-        return arr
+        return arr.sort()
                 
